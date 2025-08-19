401f2f74580f3c0a84c86659244e13e088290b74
AnaKCO <129022855+AnaKCO@users.noreply.github.com>
2025-08-18 18:45:56 -0600

    Add files via upload

---
+++ holamundo.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+            Console.ReadKey(); // Pausa la consola para ver la salida
+        }
+    }
+}
